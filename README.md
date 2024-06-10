a75d5630f2f2d452160ae1e2137fccc760a0209d
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-06-10 11:01:18 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Base
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/ack.jsx
+++
@@ -1,8 +0,0 @@
-import History from './history.jsx';
-
-export default function Ack({ ackHistory, clearHistory }) {
-
-  return (
-    <History data={ackHistory} title="Messages" stack="ack" clearHistory={clearHistory} />
-  );
-}
